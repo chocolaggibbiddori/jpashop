155a9c8115bb43e00a406192d7126bfa561f620b
chocolaggibbiddori <didrkd684@naver.com>
2023-04-21 16:31:33 +0900

    Feat : 주문 검색 기능 개발

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -3,8 +3,11 @@ package jpabook.jpashop.repository;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Repository
@@ -18,5 +21,36 @@ public class OrderRepository {
 
     public Order findOne(Long id) {
         return em.find(Order.class, id);
+    }
+
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        String jpql = "SELECT o FROM Order o JOIN o.member m";
+        boolean isFirstCondition = true;
+
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else jpql += " and";
+            jpql += " o.status = :status";
+        }
+
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else jpql += " and";
+            jpql += " m.name LIKE :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class).setMaxResults(1000);
+
+        if (orderSearch.getOrderStatus() != null)
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+
+        if (StringUtils.hasText(orderSearch.getMemberName()))
+            query = query.setParameter("name", orderSearch.getMemberName());
+
+        return query.getResultList();
     }
 }
