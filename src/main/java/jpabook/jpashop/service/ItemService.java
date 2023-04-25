50573e9979e6729c163beeb16efcc4bc92980233
chocolaggibbiddori <didrkd684@naver.com>
2023-04-25 13:12:04 +0900

    Feat : 상품 목록 조회

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -24,7 +24,7 @@ public class ItemService {
         return itemRepository.findOne(itemId);
     }
 
-    public List<Item> findItem() {
+    public List<Item> findItems() {
         return itemRepository.findAll();
     }
 }
